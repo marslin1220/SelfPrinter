cfb806c89ceddfa997f861d6fbe14b969ef6ed55
Cheng Lung Lin <marslin@gmail.com>
2018-06-25 21:44:55 +0800

    Initial Commit

---
+++ SelfPrinter/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SelfPrinter
+//
+//  Created by Lin Cheng Lung on 2018/6/25.
+//  Copyright © 2018 Lin Cheng Lung. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
